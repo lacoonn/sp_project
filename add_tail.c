7f444715ec374b017ddb0f34331bc3ad4fe7d36b
Woochan <teakan7179@gmail.com>
2016-12-14 21:17:50 +0900

    I increased the increasing length of tail
    Signed-off-by: Woochan <teakan7179@gmail.com>

--- add_tail.c
+++ add_tail.c
@@ -3,15 +3,22 @@
 #include <time.h>
 void add_tail(void)
 {
-	int temp_ch,num=0,x,y;
+	int temp_ch,num = 0,x,y;
 	struct snake *temp;
-	temp=(struct snake *)malloc(sizeof(struct snake));//새로운 꼬리 생성
-	last_tail->blink=temp;//뱀의 마지막 꼬리에 새로운 꼬리 붙치기
-	temp->flink=last_tail;//temp의 앞의 포인터를 last_tail(뱀꼬리)연결
-	temp->blink=NULL;//꼬리 끝을 표시
-	last_tail=temp;
-	last_tail->x_pos=last_tail->flink->x_pos;
-	last_tail->y_pos=last_tail->flink->y_pos;
-	mvaddch(last_tail->y_pos,last_tail->x_pos,BODY);
+	temp = (struct snake *)malloc(sizeof(struct snake));//새로운 꼬리 생성
+	last_tail->blink = temp;//뱀의 마지막 꼬리에 새로운 꼬리 붙치기
+	temp->flink = last_tail;//temp의 앞의 포인터를 last_tail(뱀꼬리)연결
+	temp->blink = NULL;//꼬리 끝을 표시
+	last_tail = temp;
+	last_tail->x_pos = last_tail->flink->x_pos;
+	last_tail->y_pos = last_tail->flink->y_pos;
+
+	temp = (struct snake *)malloc(sizeof(struct snake));
+	last_tail->blink = temp;
+	temp->flink = last_tail;
+	temp->blink = NULL;
+	last_tail = temp;
+	last_tail->x_pos = last_tail->flink->x_pos;
+	last_tail->y_pos = last_tail->flink->y_pos;
 }
 
